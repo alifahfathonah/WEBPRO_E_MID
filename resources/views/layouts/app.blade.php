7dc75c98f846c05ba6e015e0f5cbdc0e556aff24
KresnaAP <kresnaadhipramana5758@gmail.com>
2020-10-28 09:02:55 +0700

    add left navbar options

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,7 +33,19 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
+                        @guest
 
+                        @else
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{url('/forum')}}">{{ __('Forum') }}</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{url('/question')}}">{{ __('My Question') }}</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{url('/answer')}}">{{ __('My Answer') }}</a>
+                            </li>
+                        @endguest
                     </ul>
 
                     <!-- Right Side Of Navbar -->
